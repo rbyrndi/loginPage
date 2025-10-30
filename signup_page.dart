bd2733a654b71895d5be2d93adc87cda0ba0065f
Robby Riandi <rbyrndi@gmail.com>
2025-10-30 11:05:40 +0700

    Add files via upload

---
+++ signup_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: const Center(
+        child: Text('Halaman Pendaftaran'),
+      ),
+    );
+  }
+}
